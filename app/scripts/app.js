aba2c08b6ea40e57d87dc1d85d488c7d7e710cd2
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-22 08:45:37 -0700

    looping over array of objects in ng-repeat.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -30,12 +30,44 @@ angular
       .otherwise({
         redirectTo: '/'
       });
-  }).controller('MyMainCtrl',[function(){
-     var self = this;
-    self.welcomeMsg="Welcome to Angular Application Using ControllerMsg!!";
-    var notShowedMsg="This Message will not be shown";
+  }).controller('MyMainCtrl', [function () {
+    var self = this;
+    self.welcomeMsg = "Welcome to Angular Application Using ControllerMsg!!";
+    var notShowedMsg = "This Message will not be shown";
 
-    self.changeMsg=function(){
-      self.welcomeMsg=" Now The Welcome Msg changed";
-    }
+    self.changeMsg = function () {
+      self.welcomeMsg = " Now The Welcome Msg changed";
+    };
+
+    self.notes = [
+      {id: 1, label: 'First Note', done: false,asignee:'sai'},
+      {id: 2, label: 'Second Note', done: false,asignee:'ram'},
+      {id: 3, label: 'Done Note', done: true},
+      {id: 4, label: 'Last Note', done: false}
+    ];
+
+    self.getNoteClass=function(status){
+      return {
+        done:status,
+        pending:!status
+      };
+    };
+
+    self.notesObj = {
+      shyam: {
+        id: 1,
+        label: 'First Note',
+        done: false
+      },
+      Misko: {
+        id: 3,
+        label: 'Finished Third Note',
+        done: true
+      },
+      brad: {
+        id: 2,
+        label: 'Second Note',
+        done: false
+      }
+    };
   }]);
